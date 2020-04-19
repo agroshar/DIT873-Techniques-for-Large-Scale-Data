150dfcd97a2a593e1076ab7a33bae867e45f6372
juliaszulc <julszulc@gmail.com>
2020-04-19 23:24:26 +0200

    time measuring

--- Assignment2/plot_performance.py
+++ Assignment2/plot_performance.py
@@ -3,22 +3,25 @@ import time
 from matplotlib import pyplot as plt
 
 
-def run_experiment(cores_n, accuracy):
+def run_experiment(cores_n):
     start = time.time()
-    subprocess.call(['python3', 'problem2-1.py',
-                     '-w', str(cores_n),
-                     '-a', str(accuracy)])
+
+    # command = 'python3 problem2-1.py -a 0.00001 -w {}'.format(cores_n)
+    command = 'python3 problem2-2d.py -k 4 -s 100000 --c 4 -w {}'.format(cores_n)
+
+    subprocess.call(command.split())
+
     end = time.time()
 
     timespan = end - start
     return timespan
 
 
-def run_all(cores, accuracy):
+def run_all(cores):
     performance = dict.fromkeys(cores)
 
     for n in performance.keys():
-        performance[n] = run_experiment(n, accuracy)
+        performance[n] = run_experiment(n)
         
     speedup = dict.fromkeys(cores)
     baseline = performance[1]
@@ -42,8 +45,7 @@ def plot(speedup):
 
 def main():
     cores = [1, 2, 4, 8, 16, 32]
-    accuracy = 1e-5
-    speedup = run_all(cores, accuracy)
+    speedup = run_all(cores)
     plot(speedup)
 
 
--- Assignment2/problem2-2d.py
+++ Assignment2/problem2-2d.py
@@ -10,13 +10,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 import time
+import multiprocessing as mp
 from statistics import mean
 
 
 def generateData(n, c):
     logging.info(f"Generating {n} samples in {c} classes")
-    X, y = make_blobs(n_samples=n, centers=c, cluster_std=1.7, shuffle=False,
+    X, _ = make_blobs(n_samples=n, centers=c, cluster_std=1.7, shuffle=False,
                       random_state=2122)
+
     return X
 
 
@@ -27,7 +29,45 @@ def nearestCentroid(datum, centroids):
     return np.argmin(dist), np.min(dist)
 
 
-def kmeans(k, data, nr_iter=100):
+def workerTask(worker_data):
+    data_chunk, centroids = worker_data
+
+    n = len(data_chunk)
+    k = len(centroids)
+
+    cluster_sizes = np.zeros(k, dtype=int)
+    variation = 0
+    new_centroids = np.zeros((k, 2))
+    assignments_chunk = np.zeros(n)
+
+    for i in range(n):
+        cluster, dist = nearestCentroid(data_chunk[i], centroids)
+
+        cluster_sizes[cluster] += 1
+        variation += dist ** 2
+        assignments_chunk[i] = cluster
+
+        new_centroids[cluster] += data_chunk[i]
+
+    return cluster_sizes, variation, new_centroids, assignments_chunk
+
+
+def evaluate_results(clustering_results, k):
+    cluster_sizes_sum = np.zeros(k, dtype=int)
+    variation_sum = 0
+    new_centroids_sum = np.zeros((k, 2))
+    assignments = []
+
+    for (cluster_sizes, variation, new_centroids, assignments_chunk) in clustering_results:
+        cluster_sizes_sum += cluster_sizes
+        variation_sum += variation
+        new_centroids_sum += new_centroids
+        assignments += assignments_chunk.tolist()
+
+    return cluster_sizes_sum, variation_sum, new_centroids_sum, assignments
+
+
+def kmeans(k, data, nr_iter=100, workers=1):
     N = len(data)
 
     # Choose k random data points as centroids
@@ -39,40 +79,32 @@ def kmeans(k, data, nr_iter=100):
 
     logging.info("Iteration\tVariation\tDelta Variation")
     total_variation = 0.0
-    clustering_times, updating_times = [], []
+    times = []
+
     for j in range(nr_iter):
         logging.debug("=== Iteration %d ===" % (j + 1))
+        start_part_time = time.perf_counter()
 
-        # Assign data points to nearest centroid
-        variation = np.zeros(k)
-        cluster_sizes = np.zeros(k, dtype=int)
-        start_clustering_time = time.perf_counter()
-        for i in range(N):
-            cluster, dist = nearestCentroid(data[i], centroids)
-            c[i] = cluster
-            cluster_sizes[cluster] += 1
-            variation[cluster] += dist ** 2
-        clustering_times.append(time.perf_counter() - start_clustering_time)
+        workers_data = [(data_chunk, centroids) for data_chunk in np.array_split(data, workers)]
+        processes = mp.Pool(workers)
+        clustering_results = processes.map(workerTask, workers_data)
+
+        cluster_sizes_sum, variation_sum, new_centroids_sum, c = evaluate_results(clustering_results, k)
+
         delta_variation = -total_variation
-        total_variation = sum(variation)
+        total_variation = variation_sum
         delta_variation += total_variation
         logging.info("%3d\t\t%f\t%f" % (j, total_variation, delta_variation))
 
-        # Recompute centroids
-        centroids = np.zeros((k, 2))  # This fixes the dimension to 2
-        start_updating_time = time.perf_counter()
-        for i in range(N):
-            centroids[c[i]] += data[i]
-        centroids = centroids / cluster_sizes.reshape(-1, 1)
-        updating_times.append(time.perf_counter() - start_updating_time)
+        centroids = new_centroids_sum / cluster_sizes_sum.reshape(-1, 1)
 
-        logging.debug(cluster_sizes)
+        times.append(time.perf_counter() - start_part_time)
+
+        logging.debug(cluster_sizes_sum)
         logging.debug(c)
         logging.debug(centroids)
 
-    print('Average clustering time: {}'.format(mean(clustering_times)))
-    print('Average updating time: {}'.format(mean(updating_times)))
-    
+    print('Average time measured: {}'.format(mean(times)))
     return total_variation, c
 
 
@@ -85,12 +117,9 @@ def computeClustering(args):
     X = generateData(args.samples, args.classes)
 
     start_time = time.time()
-    #
-    # Modify kmeans code to use args.worker parallel threads
-    total_variation, assignment = kmeans(args.k_clusters, X, nr_iter=args.iterations)
-    #
-    #
+    total_variation, assignment = kmeans(args.k_clusters, X, nr_iter=args.iterations, workers=args.workers)
     end_time = time.time()
+
     logging.info("Clustering complete in %3.2f [s]" % (end_time - start_time))
     print(f"Total variation {total_variation}")
 
@@ -111,7 +140,7 @@ if __name__ == "__main__":
     parser.add_argument('--workers', '-w',
                         default='1',
                         type=int,
-                        help='Number of parallel processes to use (NOT IMPLEMENTED)')
+                        help='Number of parallel processes to use')
     parser.add_argument('--k_clusters', '-k',
                         default='3',
                         type=int,
